6f71c0a6bb712191b9c6cc3a809bacb61ecb8459
HristinaBuyanova <hristina-a@mail.ru>
2024-03-22 21:12:16 +0300

    Any changes

--- SimulatorWithTeam/Sources/Scenes/ViewController.swift
+++ SimulatorWithTeam/Sources/Scenes/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+
+//        какие то изменения 
     }
 
 
