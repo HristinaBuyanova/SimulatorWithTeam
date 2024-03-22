698ed41a5ba6198acd0614ae0c7eaf80e6fc00f7
HristinaBuyanova <hristina-a@mail.ru>
2024-03-22 20:42:30 +0300

    Change color VC

--- SimulatorWithTeam/Sources/Scenes/ViewController.swift
+++ SimulatorWithTeam/Sources/Scenes/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .yellow
     }
 
 
