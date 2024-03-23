ee82ab2f45af9589ec2190b6dd9b89540124b33f
sofiigorevna <sofia.konchencko@yandex.ru>
2024-03-23 13:28:35 +0300

    Create Title viewController

--- SimulatorWithTeam/Sources/Scenes/ViewController.swift
+++ SimulatorWithTeam/Sources/Scenes/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        title = "Hello students"
 
 //        какие то изменения
 //        еще изменения
